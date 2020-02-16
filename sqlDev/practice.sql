b7ede8c0a35ea9c867549743689aed4c87ca5a39
Angel <rogerangelrrp@gmail.com>
2020-02-16 09:05:15 +0100

    start

---
+++ sqlDev/practice.sql
@@ -0,0 +1,5 @@
+create table patient as select * from comqhx1.patient;
+create table drug as select * from comqhx1.drug;
+create table dose as select * from comqhx1.dose;
+
+select * from comqhx1.patient; 
